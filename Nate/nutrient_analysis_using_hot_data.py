661ef8beb29da35a0424a21f376d7487ec8feffd
nlauffenburger <nathan.lauffenburger@noaa.gov>
2022-08-17 16:19:17 -0700

    Adding TNSE plots to show clusters and histograms of each cluster to describe chlorophyll

--- Nate/nutrient_analysis_using_hot_data.py
+++ Nate/nutrient_analysis_using_hot_data.py
@@ -33,7 +33,7 @@ for j in np.arange(len(cols)):
     if j>0: 
         axs[j].set_yticklabels([])
 axs[0].set_ylabel('Pressure [dbar]')
-plt.show()
+plt.savefig('Features')
 
 # How many individual measurements do we have
 len(data)
@@ -49,24 +49,53 @@ K=4 # number of clusters picked from below
 gmm = GaussianMixture(n_components=K)
 labels_gmm=gmm.fit_predict(features)
 x_mean,y_mean=gmm.means_[:,0],gmm.means_[:,1]
+data['labels'] = labels_gmm
 
 fig = plt.figure(figsize=(10,10))
 plt.title('Gaussian Mixture Model clustering')
 for k in range(K):
     plt.plot(x_mean[k],y_mean[k],'rs',markersize=6)
     plt.annotate(str(k), (x_mean[k],y_mean[k]), fontsize=20)    
-plt.show()
+plt.savefig('GMM_clustering.png')
 
-BIC=np.array([]);
-for K in range(1,50):
-    gmm = GaussianMixture(n_components=K)
-    gmm.fit_predict(features)
-    bic=gmm.bic(features)
-    BIC=np.append(BIC,bic)
+#BIC=np.array([])
+#for K in range(1,50):
+#    gmm = GaussianMixture(n_components=K)
+#    gmm.fit_predict(features)
+#    bic=gmm.bic(features)
+#    BIC=np.append(BIC,bic)
+#fig = plt.figure(figsize=(10,10))
+#plt.title('BIC')
+#plt.plot(BIC); 
+#plt.xlabel('Number of classes'); 
+#plt.ylabel('Bayesian information criterion (BIC)')
+#plt.show()
+
+tsne = TSNE(n_components=2, verbose=1)
+z = tsne.fit_transform(features) 
+data['X'] = z[:, 0]
+data['Y'] = z[:, 1]
+
+color_list = ['darkorange', 'forestgreen', 'slateblue', 'lightcoral']
 fig = plt.figure(figsize=(10,10))
-plt.title('BIC')
-plt.plot(BIC); 
-plt.xlabel('Number of classes'); 
-plt.ylabel('Bayesian information criterion (BIC)')
-plt.show()
+all_labels = pd.unique(data['labels'])
+for L in all_labels:
+    x = data['X'][data['labels']==L]
+    y = data['Y'][data['labels']==L]
+    plt.scatter(x, y, color=color_list[L])
+plt.xlabel('Comp-1')
+plt.ylabel('Comp-2')
+plt.title('Nutrient data T-NSE projection')
+plt.savefig('2D_nutrient_classes')
+
+fig, axs=plt.subplots(4, 1,figsize=(4, 12))
+plt.title('Chlorophyll abundance by class')
+axs[0].hist(data['chl_bottle_hot'][data['labels']==all_labels[0]], color=color_list[0], bins=30)
+axs[1].hist(data['chl_bottle_hot'][data['labels']==all_labels[1]], color=color_list[1], bins=30)
+axs[2].hist(data['chl_bottle_hot'][data['labels']==all_labels[2]], color=color_list[2], bins=30)
+axs[3].hist(data['chl_bottle_hot'][data['labels']==all_labels[3]], color=color_list[3], bins=30)
+xlabel('')
+plt.savefig('Chl_histogram.png')
+
+    
 
